b30968d1a26afab13b8faeeae36d60359484ab13
Wilson Dev <hch.wilson@hynkor.com>
2025-02-20 23:50:01 -0500

    ✨ feat(fundamentos): add booleans

---
+++ fundamentos/01_basic/06_booleans.py
@@ -0,0 +1,13 @@
+# Boolean
+
+is_developer = True
+print(type(is_developer)) # output: boolean
+
+is_single = False
+print(type(is_developer)) # output: boolean
+
+print(not True)  # output: False
+print(not False) # output: True
+
+is_developer = not is_developer
+print(is_developer) # output: True
