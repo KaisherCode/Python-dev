b22af7fda78b68ddf1b7cccae0bc726cdb0c2531
Wilson Dev <hch.wilson@hynkor.com>
2025-02-21 18:03:05 -0500

    ✨ feat(fundamentos): add tuples

---
+++ fundamentos/01_basic/18_tuples.py
@@ -0,0 +1,23 @@
+# Tuples: We can't make changes to them. We using ()
+
+numbers = (1,2,3,4) # Tuple inmmutable
+strings = ('nico','zule','santi','nico') # Tuple
+print(numbers)
+print(type(numbers))
+
+print(strings)
+print(type(strings))
+
+print(numbers)
+print(strings.index('zule')) # zule is in position 1
+print(strings.count('nicolle')) # 'nicolle' = appers twice
+
+my_list = list(strings) # convert a tuple to a list
+print(my_list)
+print(type(my_list))
+
+my_list[1] = 'juli' # Modify position 1
+print(my_list)
+
+my_tuple = tuple(my_list) # List-to-tuple convertion
+print(my_tuple)
