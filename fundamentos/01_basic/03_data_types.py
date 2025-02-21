fcbfebf9dd7fa70817c3be7b6ffd923989646588
Wilson Dev <hch.wilson@hynkor.com>
2025-02-20 23:48:47 -0500

    ✨ feat(fundamentos): add data types

---
+++ fundamentos/01_basic/03_data_types.py
@@ -0,0 +1,26 @@
+# Data types
+
+# string
+
+first_name = "Kaisher"
+last_name = 'Dev'
+print('My full name is =>', first_name, last_name)
+
+print(type(first_name)) # output: str
+# Integer
+
+my_age = 21
+print(f"I am {my_age} years old.")
+
+print(type(my_age)) # output: int
+
+# boolean
+
+ls_single = True
+print('ls_single =>', ls_single)
+
+# Inputs
+
+my_age = input("How old are you?")
+print('my_age =>', my_age)
+print(type(my_age)) # output: str
