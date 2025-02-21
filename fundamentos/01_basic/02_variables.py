d40abac9085fe9e681021cc045cddd5369c81d90
Wilson Dev <hch.wilson@hynkor.com>
2025-02-20 23:48:23 -0500

    ✨ feat(fundamentos): add variables

---
+++ fundamentos/01_basic/02_variables.py
@@ -0,0 +1,19 @@
+# Great
+print("Hello, Kaisher")
+
+# Variable str
+my_name = "Kaisher"
+
+print(f"My name is: {my_name}")
+
+# variable int
+my_age = 25
+
+print(f"I am {my_age} years old")
+
+# Variable str
+my_name = "Dev"
+print("Here changed", my_name)
+
+my_name = input("What your name?: ")
+print("New name using input: ", my_name)
