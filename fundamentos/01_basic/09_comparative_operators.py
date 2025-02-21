c4d3d981b3a132f7dfd799afb514647dcc3456a9
Wilson Dev <hch.wilson@hynkor.com>
2025-02-21 18:00:00 -0500

    ✨ feat(fundamentos): add comparatice operator

---
+++ fundamentos/01_basic/09_comparative_operators.py
@@ -0,0 +1,36 @@
+# Comparative operators
+
+# > Mayor que
+print(7>3)
+print(3>7)
+print(7>7)
+
+# < less than - Menor que
+print(5<6)
+print(6<5)
+
+# >= Greather than or equal to - Mayor o igual que
+print(2>=1)
+print(2>=3)
+print(2>=2)
+
+# <= Less than or iqual to - Menor o igual que
+print(1<=2)
+print(2<=1)
+print(2<=2)
+
+# == Strict equal - Estrictamente de igualdad
+print(6 ==6)
+print(5==2)
+
+
+# != Different from - Diferente de
+print(6 != 10)
+print(6 != 6)
+
+print("Apple" == 'Apple')
+print("Apple" == 'apple')
+print("1"==1)
+
+age = 21
+print(age >= 18)
