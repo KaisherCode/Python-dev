4454e7ca0ac53ec53c2802d2129c4062f0f4e76f
Wilson Dev <hch.wilson@hynkor.com>
2025-02-20 23:49:42 -0500

    ✨ feat(fundamentos): add numbers

---
+++ fundamentos/01_basic/05_numbers.py
@@ -0,0 +1,11 @@
+# Numbers
+
+lives = 10
+age = 25
+weight = 72.45
+temperature = 35
+
+print(type(lives))  # output: int
+print(type(age))    # output: int    
+print(type(weight)) # output: int
+print(type(temperature)) # output: float
