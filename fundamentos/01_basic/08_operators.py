a3f351c41298e2e07cba6e003fd6240c574e36a6
Wilson Dev <hch.wilson@hynkor.com>
2025-02-21 17:59:32 -0500

    ✨ feat(fundamentos): add operators

---
+++ fundamentos/01_basic/08_operators.py
@@ -0,0 +1,43 @@
+# Operators
+
+# Sum
+print(10+10)
+
+# Less
+print(10-5)
+
+# Multipliply
+print(10*2)
+
+# Division
+print(10/2) # 5
+
+# Module - even number
+print(10 % 2) # 0
+
+# 
+print(10/3)
+
+# Module - odd number 
+print(10 % 3) # 1
+
+print(10//3) # 3
+
+# Exponents
+print(2**3) # 8
+
+print(2**3 + 3 -7/1//4)
+
+# print(10/0) => Can not be divided by 0
+
+print('Hello' + ' World')
+print('Hello ' *3)
+
+# ====Acrostic PENDAS===
+
+# P => Parentheses
+# E => Exponents
+# M => Multiplication
+# D => Division
+# A => Adition
+# S => Subtraction
