645ec35739a5a2b26c232cc537a1fd37978c06e0
Wilson Dev <hch.wilson@hynkor.com>
2025-02-21 11:47:53 -0500

    ✨ feat(fundamentos): add transformations

---
+++ fundamentos/01_basic/07_transformation.py
@@ -0,0 +1,26 @@
+# transformations
+
+name = "Kaisher"
+print(type(name)) # output: str
+
+number = 12
+print(type(number)) # output: int
+
+is_true = True
+print(type(is_true)) # output: boolean
+
+# concatenate
+print("Kaisher"+ "Dev") 
+print(10+20) # putput: 30
+print("Kaisher" + "12")
+
+
+age = 25
+print("My age is: "+ str(age))
+print(f"My age is: {age}")
+
+age = input("Enter your age: =>")
+print(type(age))
+age = int(age)
+age += 10
+print(f"Your age in will be: {age}")
