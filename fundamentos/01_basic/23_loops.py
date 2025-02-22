bff484044e0f8fdf2ad05453a02ed3c191d611eb
Wilson Dev <hch.wilson@hynkor.com>
2025-02-21 21:22:29 -0500

    ✨ feat(fundamentos): add loops

---
+++ fundamentos/01_basic/23_loops.py
@@ -0,0 +1,18 @@
+# Annidados Cycles | loops
+
+matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+print(matriz[0])
+print(matriz[0][1])
+
+for element in matriz:
+    print(element)
+
+for element in matriz:
+    print(element)
+    for item in element:
+        print(item)
